6e92e183e5e4d4c097da1f1589247d2c35f68505
Romina Solange Botana Recio <rominabotana@outlook.com>
2024-05-21 00:42:22 -0300

    ejercicios

--- par-impar.ts
+++ par-impar.ts
@@ -2,16 +2,16 @@ function chequearNumero (numero: number):
 string {
     if (numero == 0){
         return "El numero es 0.";
-    } else if (numero % 5 == 0){
+    } else if (numero % 2 == 0){
         return "El numero es par.";
     } else {
         return "El numero es impar.";
     }
 }
 //ejercicio
-console.log(chequearNumero(5));
-console.log(chequearNumero(2));
+console.log(chequearNumero(7));
+console.log(chequearNumero(4));
 console.log(chequearNumero(0));
-console.log(chequearNumero(3));
+
 
 
